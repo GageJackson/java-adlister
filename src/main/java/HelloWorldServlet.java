202bdf880cea69d894bc88aa3891c8e2edafc237
GageJackson <122929065+GageJackson@users.noreply.github.com>
2023-04-27 14:25:45 -0500

    finished servlets

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,32 @@
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+@WebServlet(name="HelloWorldServlet", urlPatterns = "/hello")
+//servlet is responsible for ANY requests sent to
+//baseURLpattern/hello-world
+//for out local machines:
+//localhost:8080/hello-world //8080 is a http port
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+    //@Override
+    int count = 0;
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        count += 1;
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        String sayHelloTo = request.getParameter("name");
 
+        if(sayHelloTo != null && sayHelloTo.equals("zero")){
+            count = 0;
+            out.println("<h1>Goodbye, Cruel World</h1>");
+            out.println("<p>" + count + "</p>");
+        } else {
+            out.println("<h1>Hello, World</h1>");
+            out.println("<p>" + sayHelloTo + "</p>");
+            out.println("<p>" + count + "</p>");
+        }
     }
 }
